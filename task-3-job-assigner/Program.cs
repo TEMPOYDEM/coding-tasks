71d51829a3fdd47a67b2d91834f593327882b210
Артем Каравашкин <karavashkinav.20@edu.ystu.ru>
2021-06-17 10:39:26 +0300

    .

--- task-3-job-assigner/Program.cs
+++ task-3-job-assigner/Program.cs
@@ -13,20 +13,23 @@ namespace task_3_job_assigner
             Console.WriteLine("Введите количество заданий для выполнения");
             int[] tasks = new int[Convert.ToInt32(Console.ReadLine())];
             int[,] work = new int[tasks.Length,tasks.Length];
+            int[] best = new int[tasks.Length];
+
             for (int i = 0; i < tasks.Length; i++)
             {
+                int[] comparison = new int[tasks.Length];
                 for (int f = 0; f < tasks.Length; f++)
                 {
                     Console.WriteLine("Введите цену задания " + (f + 1) + " для работника " + (i + 1));
                     int price = Convert.ToInt32(Console.ReadLine());
                     work[i, f] = price;
+                    comparison[f] = price;
                 }
+                best[i] = comparison.Min();
             }
-            int[] comparison = new int[tasks.Length];
-            for (int i = 0; i<tasks.Length;i++)
+            for(int i = 0; i < tasks.Length; i++)
             {
-                var max = Enumerable.Range(0, work.GetLength(1)).Max(best => work[1, best]);
-                Console.WriteLine(max);
+                Console.WriteLine(best.ElementAt(i));
             }
 
             Console.ReadLine();
